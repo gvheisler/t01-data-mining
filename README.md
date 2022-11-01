c9ff398bfd107d7f0f71a81719b92c3486865e82
gvheisler <gvheisler@inf.ufsm.br>
2022-11-01 19:58:36 -0300

    adicionada coluna vitoria/derrota

--- README.md
+++ README.md
@@ -6,5 +6,8 @@
 Transformar CSVs em one hot encoding
 
 
+### O que foi feito:
+Adicionado no data frame uma coluna para vitoria ou derrota
+
 ### Anotações:
 Empate na linha 17
--- main_gvh.r
+++ main_gvh.r
@@ -4,8 +4,13 @@ df <- rbind(read.csv(url("http://www-usr.inf.ufsm.br/~joaquim/UFSM/DM/ds/_ASSOC_
 
 df <- df[,-1]
 
+res <- vector("numeric")
 for (i in 1:nrow(df)) {
-  if(df[i,]$Oponentes==df[i,]$Amigos){
-    print(i)
+  if(df[i,]$Oponentes>df[i,]$Amigos){
+    res <- c(res, 0)
+  }else{
+    res <- c(res, 1)
   }
 }
+
+df <- cbind(df, res)
